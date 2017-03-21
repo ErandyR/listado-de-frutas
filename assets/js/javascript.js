86b4ab30089f818eaab1e5bcdf62e2b4eedb7b04
ErandyR <rerandy15@gmail.com>
2017-03-20 21:04:33 -0600

    listado de frutas interactivo

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,21 @@
-alert("vamos a vender frutas!!")
+function agregarFruta(){
+  var frutaAgregar = document.getElementById("frutita").value;
+  if(frutaAgregar.length > 0){
+    if(find_li(frutaAgregar)){
+      var li = document.createElement('li');
+      li.id = frutaAgregar;
+      li.innerHTML = frutaAgregar;
+      document.getElementById("agregandoFruta").appendChild(li);
+    }
+  }
+  return false;
+}
+
+function find_li(contenido){
+  var fruit = document.getElementById("agregandoFruta").getElementsByTagName("li");
+  for (var i = 0; i < fruit.length; i++){
+    if(fruit[i].innerHTML == contenido)
+    return false;
+  }
+  return true;
+}
